b5ae83e92affbb78c1165f7817f8f7bf910c16ca
Javi <54064297+Javiihg@users.noreply.github.com>
2024-05-10 19:25:20 +0100

    Limite de velocidad y agua

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float moveSpeed = 2f;
+    public float maxDistanceSpeed = 0.1f; //distacia maxima q puede moverse
     private Camera mainCamera;
 
     void Start()
@@ -26,7 +27,19 @@ public class Movement : MonoBehaviour
             {
                 Vector3 targetPosition = hit.point;
                 targetPosition.y = transform.position.y;  
-                transform.position = Vector3.Lerp(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+                Vector3 newPosition = Vector3.Lerp(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+                
+                //Limitar la distancia q puede moverse en el frame
+
+                float distanceToMove = Vector3.Distance(transform.position, newPosition); //calcular la dsitancia q se moverá
+
+                //si la distancia es mayor q maxDistanceSpeed, aplica el codigo de maxds
+                if (distanceToMove > maxDistanceSpeed) 
+                {
+                    newPosition = transform.position + (newPosition - transform.position).normalized * maxDistanceSpeed;
+                }
+
+                transform.position = newPosition;
             }
         }
     }
